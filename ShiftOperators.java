f079ec01229940501666d706f070cfed52f41d2b
Mounika <nagargojemounika@gmail.com>
2025-01-28 22:05:44 +0530

    Add files via upload

---
+++ ShiftOperators.java
@@ -0,0 +1,17 @@
+public class ShiftOperators {
+        public static void main(String[] args) {
+            // Left Shift Operator (<<)
+            int num = 10;
+            System.out.println("Original number: " + num);
+            System.out.println("Left shift by 2 bits: " + (num << 2));
+            System.out.println("Left shift by 3 bits: " + (num << 3));
+    
+            // Right Shift Operator (>>)
+            num = 40;
+            System.out.println("\nOriginal number: " + num);
+            System.out.println("Right shift by 2 bits: " + (num >> 2));
+            System.out.println("Right shift by 3 bits: " + (num >> 3));
+        }
+    }
+    
+
